38ac7ca9cd86c4e00f8e64dbcd3370bd76d3192e
Rishav <rishavbarman.dev@gmail.com>
2026-01-28 15:30:40 +0530

    Initial project setup

---
+++ frontend/app/layout.js
@@ -0,0 +1,33 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+
+const  inter = Inter({ subsets: ["latin"]});
+
+export const metadata = {
+  title: "Served - AI Recipe Platform",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning={true}>
+      <body
+        className={`${inter.className}`}
+      >
+        <Header />
+        <main className="min-h-screen">
+          {children}
+        </main>
+        <footer className="py-8 px-4 border-t">
+          <div className="max-w-6xl mx-auto flex justify-center">
+            <p className="text-stone-500 text-sm">
+
+            Made with ❤️ by Served
+            </p>
+          </div>
+        </footer>
+      </body>
+    </html>
+  );
+}
