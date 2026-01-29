74294bd3a42d170d2420c2194d5c53f2c8234b99
Rishav <rishavbarman.dev@gmail.com>
2026-01-29 18:14:22 +0530

    feat: update header component

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -7,9 +7,10 @@ import {
 } from "@clerk/nextjs";
 import React from "react";
 import { Button } from "./ui/button";
-import { Cookie, Link, Refrigerator } from "lucide-react";
+import { Cookie, Refrigerator } from "lucide-react";
 import Image from "next/image";
 import UserDropdown from "./UserDropdown";
+import Link from "next/link";
 
 const Header = async () => {
     const user = null; // Replace with actual user fetching logic
@@ -18,7 +19,7 @@ const Header = async () => {
     <header className="fixed top-0 w-full border-b border-stone-200 bg-stone-50/80 backdrop-blur-md z-50 supports-backdrop-filter:bg-stone-50/60 px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-end space-x-4">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-end space-x-4">
         <Link href={user ? "/dashboard" : "/"} className="mr-auto">
-          <Image src="/orange-logo.png" alt="RecipeAI Logo" width={60} height={60} className="w-16 h-16" />
+          <Image src="/orange-logo.png" alt="RecipeAI Logo" width={60} height={60} className="w-16 h-14" />
         </Link>
         <div className="hidden md:flex items-center space-x-8 text-sm font-medium text-stone-600">
             <Link 
