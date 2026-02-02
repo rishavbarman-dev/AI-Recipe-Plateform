7cd1dc41a6be45c52b40e685ed1d44325f780575
Rishav <rishavbarman.dev@gmail.com>
2026-02-02 20:58:05 +0530

    chore: update dependencies and next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  experimental: {
+    serverComponentsHmrCache: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.themealdb.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      }
+    ],
+  }
 };
 
 export default nextConfig;
