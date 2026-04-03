7762cfb7015966a6ae994b32ebe9891413e52e7a
Rishav <rishavbarman.dev@gmail.com>
2026-04-03 12:17:24 +0530

    adding Arcjet shield and Bot protection

--- frontend/lib/arcjet.js
+++ frontend/lib/arcjet.js
@@ -1,8 +1,23 @@
-import arcjet, { tokenBucket } from "@arcjet/next";
+import arcjet, { detectBot, shield, tokenBucket } from "@arcjet/next";
 
 export const aj = arcjet({
     key: process.env.ARCJET_KEY,
-    rules: [],
+    rules: [
+      shield({
+        mode: "LIVE",
+      }),
+      detectBot({
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      // Block all bots except the following
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+        // Uncomment to allow these other common bot categories
+        // See the full list at https://arcjet.com/bot-list
+        //"CATEGORY:MONITOR", // Uptime monitoring services
+        //"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
+      ],
+    }),
+    ],
 });
 
 // Free tier pantry scan limits (10 scans per month)
--- frontend/proxy.js
+++ frontend/proxy.js
@@ -1,5 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
+import { aj } from './lib/arcjet';
 
 const isProtectedRoute = createRouteMatcher([
     "/recipe(.*)",
@@ -9,6 +10,12 @@ const isProtectedRoute = createRouteMatcher([
 ])
 
 export default clerkMiddleware(async (auth, req) => {
+    const decision = await aj.protect(req);
+
+    if(decision.isDenied()) {
+      return NextResponse.json({error: "Forbidden"}, {status:403});
+    }
+
     const {userId, redirectToSignIn} = await auth();
     if (!userId && isProtectedRoute(req)) {
         return redirectToSignIn();
